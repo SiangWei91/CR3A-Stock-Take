4a7136b1b4f7eff4bf3510567d5270ec8efb7af1
SiangWei91 <siangwei91@gmail.com>
2024-12-26 11:16:46 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'CR3A-Stock-Take-cache-v1.3';
+const CACHE_NAME = 'CR3A-Stock-Take-cache-v1.4';
 const urlsToCache = [
     '/CR3A-Stock-Take/',
     '/CR3A-Stock-Take/index.html',
